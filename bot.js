7123dce520092cea746d4000393f0b8779b2a794
Abdullah <alkhamsi.abdullah@gmail.com>
2018-12-14 00:25:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -221,7 +221,7 @@ function play(guild, song) {
     const serverQueue = queue.get(guild.id);
  
     if (!song) {
-//         serverQueue.voiceChannel.leave();
+        serverQueue.voiceChannel.leave();
         queue.delete(guild.id);
         return;
     }
